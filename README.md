a7b0c1f4449136911c30398dd20181c4f4bd28bb
Marat Talipov <t-marat@yandex.ru>
2021-03-09 12:27:12 +0500

    init

---
+++ README.md
@@ -0,0 +1,29 @@
+# googledoc-extact-images
+Утилита извлекает изображения из Гугл-дока, складывает в одну папку и нумерует в порядке использования в документе.
+Особенно полезна, когда в документе много изображений.
+
+
+## Демо
+![googledoc-extact-images demo](https://www.marat-talipov.ru/googledoc-extract-images/_images_for_github_readme/demo3.gif)
+
+
+## Как пользоваться
+1. Скачать архив
+1. Запустить исполняемый файл `googledoc-extract-images`
+1. Ввести ссылку на гугл-документ. Он должен быть открыт всем на чтение:
+![googledoc-extact-images share](https://www.marat-talipov.ru/googledoc-extract-images/_images_for_github_readme/share.png)
+1. Нажать Кнопку `[Извлечь]` или клавишу `Enter`
+1. В директории с исполняемым файлом появится папка `extracted-images\<текущие_дата_и_время>`. В ней находятся извлеченные изображения.
+1. Файловый менеджер автоматически откроет эту папку
+
+
+## Работает в:
+- ✅ **Windows 10**
+- ✅ **Linux**
+- ❌ **macOS**. *В теории должно, но я не тестировал*.
+
+
+## Скачать
+- **[Windows](https://www.marat-talipov.ru/googledoc-extract-images/win/googledoc-extract-images_0.1.0.7z)**
+- **[Linux](https://www.marat-talipov.ru/googledoc-extract-images/lin/googledoc-extract-images_0.1.0.7z)**
+- **macOS**. *Будет позже*
---
+++ functions.js
@@ -0,0 +1,154 @@
+const unzipper = require('unzipper');
+const fs = require('fs');
+const request = require('request');
+const os = require('os');
+var HTMLParser = require('node-html-parser');
+const { shell } = require('electron');
+
+document.querySelector('#btnEd').addEventListener('click', () => {
+
+    logger.clear();
+    logger.add('init ...');
+
+    const tmpDir = os.tmpdir() + '/' + 'google_extract_images/' + uuidv4() + '/';
+    const zipFullName = tmpDir + 'zip.zip';
+    const unzipDirFullName = tmpDir + 'unzip';
+                                // хрень, но пока оставим так
+    const dirImages = __dirname + '/../../' + 'extracted_images/' + getDate();
+    let url = document.getElementById('inputUrl').value;
+
+    logger.ok();
+    logger.add('parse url ...');
+
+    let docID;
+    url.split('/').forEach(function (item, index, array) {
+        if (item === 'd') {     // находим в url /d/ и после него идет айдишник документа
+            docID = array[index + 1];
+        }
+    });
+    let downloadUrl = 'https://docs.google.com/document/d/' + docID + '/export?format=zip';
+
+    logger.ok();
+    logger.add('create tmp dir ...');
+
+    if (!fs.existsSync(tmpDir)) {
+        fs.mkdirSync(tmpDir, { recursive: true });
+    }
+
+    logger.ok();
+    logger.add('download ' + downloadUrl + ' ...');
+
+    request(downloadUrl, function (error, response, body) {
+        if (response.statusCode != 200) return logger.append(response.statusCode);
+        logger.ok();
+        logger.add('unzip ...');
+
+        let pipeUnzip =
+            fs.createReadStream(zipFullName)
+                .pipe(unzipper.Extract({ path: unzipDirFullName }));
+
+        pipeUnzip.on('close', function () {
+            logger.ok();
+            logger.add('read html ...');
+
+            let htmlName = fs.readdirSync(unzipDirFullName).filter(file => {
+                return file.indexOf('html') > -1;
+            })[0];
+
+            fs.readFile(unzipDirFullName + '/' + htmlName, 'utf8', function (err, data) {
+                if (err) return logger.append(err);
+                logger.ok();
+
+                logger.add('create folder ...');
+                if (!fs.existsSync(dirImages)) {
+                    fs.mkdirSync(dirImages, { recursive: true });
+                }
+                logger.ok();
+
+                logger.add('parse dom ...');
+                const dom = new DOMParser().parseFromString(data, 'text/html');
+                logger.ok();
+
+                let cnt = 1; // нумерация изображений
+                logger.add('copy images ...');
+                for (let item of dom.images) {
+                    let oldFullFileName = unzipDirFullName + '/' + item.getAttribute('src');
+                    let newFullFileName = dirImages + '/' + (cnt++) + '.png';
+                    fs.copyFileSync(oldFullFileName, newFullFileName);
+                    logger.image(newFullFileName);
+                }
+
+
+                logger.add('open folder ...');
+                shell.openPath(dirImages);
+                logger.ok();
+
+                logger.add('finish');
+            });
+        });
+        pipeUnzip.on('error', function (error) {
+            logger.append(error);
+        });
+    }).pipe(fs.createWriteStream(zipFullName)); // пишем скачанное в файл
+
+});
+
+
+// при нажатии Enter в поле ввода
+document.querySelector('#inputUrl').addEventListener("keyup", function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("btnEd").click();
+    }
+});
+
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+function getDate() {
+    function pad(n) { return n < 10 ? '0' + n : n };
+    let d = new Date();
+    return d.getFullYear() + '-'
+        + pad(d.getMonth() + 1) + '-'
+        + pad(d.getDate()) + '_'
+        + pad(d.getHours()) + '-'
+        + pad(d.getMinutes()) + '-'
+        + pad(d.getSeconds());
+}
+
+// типа логгер, который будет выходить на экране.
+// Просто пишем текст в div
+let logger = {
+    // нумерация сообщений
+    number: 1,
+
+    // новая строка с новым номером
+    add: function (sText) {
+        document.querySelector('#logger').innerHTML += '<br>' + (this.number++) + '. ' + sText;
+    },
+
+    // дописываем к существующей строке
+    append: function (sText) {
+        document.querySelector('#logger').innerHTML += sText;
+    },
+
+    // дописываем к существующей строке статус ОК
+    ok: function () {
+        document.querySelector('#logger').innerHTML += 'ok';
+    },
+
+    // для изображений. новая строка, но без нумерации а с дефисом
+    image: function (sText) {
+        document.querySelector('#logger').innerHTML += '<br> - ' + sText;
+    },
+
+    // очищаем, убираем нумерацию
+    clear: function () {
+        document.querySelector('#logger').innerHTML = '';
+        this.number = 1;
+    }
+}
